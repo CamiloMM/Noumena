43ca0c6f04a2deb5a2f863609e54d9fe5d3f449b
CamiloMM <herzschaltung@yahoo.com.ar>
2015-03-10 11:58:15 -0300

    Passport module.

---
+++ app/passport.js
@@ -0,0 +1,30 @@
+var passport      = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var config        = require('../config.json');
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    var user = config.users[username];
+
+    if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+    }
+
+    if (user.pass !== password) {
+        return done(null, false, { message: 'Incorrect password.' });
+    }
+
+    done(null, user);
+}));
+
+// We're using very dumb serialization here because we don't really
+// intend on having many users at the moment (probably just admin).
+
+passport.serializeUser(function(user, done) {
+    done(null, JSON.stringify(user));
+});
+
+passport.deserializeUser(function(json, done) {
+    done(null, JSON.parse(json));
+});
+
+module.exports = passport;
