c2625b7a5b5a4a93ce1b66f2667d937880fe0605
CamiloMM <herzschaltung@yahoo.com.ar>
2015-03-31 16:17:09 -0300

    Login route for POST, that authenticates users.

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var router  = express.Router();
+var express  = require('express');
+var router   = express.Router();
+var passport = require('../app/passport');
 
 router.get('/login', function(req, res) {
-    res.render('login', { title: 'Log in' });
+    res.render('login', { title: 'Login' });
 });
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/projects',
+    failureRedirect: '/login',
+    failureFlash:    false
+}));
 
 module.exports = router;
