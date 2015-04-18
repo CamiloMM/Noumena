ad4b65753fcf7f1f0031a17ae06d9da88a4da4d5
CamiloMM <herzschaltung@yahoo.com.ar>
2015-04-18 11:43:30 -0300

    Add a nojs class to body and remove it via js.

--- scripting/src/autoload.js
+++ scripting/src/autoload.js
@@ -3,6 +3,9 @@
 // It is intended to provide fixes and whatnot.
 
 module.exports = function(utils, $, _) {
+    // Remove .nojs class from body.
+    $('body').removeClass('nojs');
+
     // Trigger change on browser auto-fill after a quarter-second.
     setTimeout(function() {
         $('input').each(function() {
