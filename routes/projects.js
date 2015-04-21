a3e57dbd1b1aab46c86d366c6408ba8ee7ce6b53
CamiloMM <herzschaltung@yahoo.com.ar>
2015-04-20 23:59:25 -0300

    Add login flash messages and redirects to /projects route

--- routes/projects.js
+++ routes/projects.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/projects', function(req, res) {
-    res.render('projects', { title: 'Projects' });
+    if (req.user && req.user.admin) {
+        res.render('projects', { title: 'Projects' });
+    } else {
+        req.flash('bad', 'You must be logged in as an admin to view that page.');
+        res.redirect('/login');
+    }
 });
 
 module.exports = router;
