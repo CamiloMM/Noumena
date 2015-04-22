c92187e7a0edce7ad7014a5bb66f1be6e9655b41
CamiloMM <herzschaltung@yahoo.com.ar>
2015-04-22 07:04:37 -0300

    Performance improvements to the static route.

--- routes/static.js
+++ routes/static.js
@@ -2,14 +2,16 @@ var express  = require('express');
 var router   = express.Router();
 var app      = require('../app.js');
 
+var types = {css: 'text/css', js: 'application/javascript'};
+var pattern = /^[a-z0-9]{32}\\.(css|js)$/;
+
 router.get('/static/:path(*)', function(req, res) {
-    var types = {css: 'text/css', js: 'application/javascript'};
 
-    for (type in types) {
+    if (pattern.test(req.params.path)) {
+        var md5 = req.params.path.substr(0, 32);
+        var type = req.params.path.substr(33);
         var compiled = app.get(type);
-        var pattern = new RegExp('^' + compiled.md5 + '\\.' + type + '$');
-
-        if (pattern.test(req.params.path)) {
+        if (compiled.md5 === md5) {
             res.set('Content-Type', types[type]);
             res.set('Cache-Control', 'public, max-age=31536000'); // A year.
             res.set('Expires', new Date(Date.now() + 31536000000).toUTCString());
