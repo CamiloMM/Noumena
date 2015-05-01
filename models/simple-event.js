b5c3796ea11c78af5f3aa01e1d57197d08ee3d6e
CamiloMM <herzschaltung@yahoo.com.ar>
2015-05-01 11:23:37 -0300

    Simple event model.

---
+++ models/simple-event.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+// A simple event is an event which does not contain data associated with it, and
+// instead just records "it happened", the count field incrementing with each hit.
+// For many events this is viable enough, and can be optimized to be way faster.
+
+var simpleEventSchema = new Schema({
+    project   : String,
+    category  : String,
+    action    : String,
+    count     : {type: Number, default: 0} // This is where the 'data' key would've been.
+});
+
+var SimpleEvent = mongoose.model('SimpleEvent', simpleEventSchema);
+
+module.exports = SimpleEvent;
