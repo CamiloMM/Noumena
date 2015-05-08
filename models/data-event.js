bb54f9ecdd46a6ab60414d53232ddffc47c59544
CamiloMM <herzschaltung@yahoo.com.ar>
2015-05-08 00:23:34 -0300

    Set up indexing for data events.

--- models/data-event.js
+++ models/data-event.js
@@ -9,10 +9,25 @@ var dataEventSchema = new Schema({
     project   : String,
     category  : String,
     action    : String,
-    count     : {type: Number, default: 0},
-    hash      : String, // MD5 hex string of data object, generated via object-hash module.
+    count     : {type: Number, default: 0, index: true},
+    // MD5 hex string of data object, generated via object-hash module.
+    hash      : {type: String, index: {unique: true}},
     data      : {} // Note this technically means mixed; we need to enforce object.
 });
+
+// Compound index because these fields are treated like namespaces.
+dataEventSchema.index({project: 1, category: 1, action: 1});
+
+// These fields may (or may not) be set by flags, so they are sparsely indexed.
+dataEventSchema.path('data.ip'         ).index({sparse: true});
+dataEventSchema.path('data.lat'        ).index({sparse: true});
+dataEventSchema.path('data.lon'        ).index({sparse: true});
+dataEventSchema.path('data.country'    ).index({sparse: true});
+dataEventSchema.path('data.time'       ).index({sparse: true});
+dataEventSchema.path('data.agent'      ).index({sparse: true});
+dataEventSchema.path('data.browser'    ).index({sparse: true});
+dataEventSchema.path('data.fingerprint').index({sparse: true});
+dataEventSchema.path('data.num'        ).index({sparse: true, unique: true});
 
 var DataEvent = mongoose.model('DataEvent', dataEventSchema);
 
