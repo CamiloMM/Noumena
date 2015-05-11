a695be626a0131d2d50bd12ae78fe64552895e2d
CamiloMM <herzschaltung@yahoo.com.ar>
2015-05-11 09:48:07 -0300

    Flash error in case db screws up.

--- routes/projects.js
+++ routes/projects.js
@@ -5,6 +5,7 @@ var db      = require('../app/db.js');
 
 router.get('/projects', app.adminOnly(function(req, res) {
     db.getProjects(function(projects) {
+        if (!projects) req.flash('bad', 'Database error from db.getProjects!');
         res.render('projects', { title: 'Projects', projects: projects });
     });
 }));
