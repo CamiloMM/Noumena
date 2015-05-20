8a64a017121b4f5e0e32f7ffe3aae3519f3e4697
CamiloMM <herzschaltung@yahoo.com.ar>
2015-05-20 12:02:25 -0300

    Check if endpoint handler exists.

--- routes/event.js
+++ routes/event.js
@@ -4,6 +4,7 @@ var endpoint = require('../app/endpoint');
 
 router.get('/event/:name/:args(*)', function(req, res) {
     var e = endpoint('http', 'get', req.params.name);
+    if (!e) return res.status(400).send('The requested endpoint has not been defined.');
     e({
         args: req.params.args,
         body: req.body
