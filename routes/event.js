4503c68cca01f804a8db30c89228244f48d3fc20
CamiloMM <herzschaltung@yahoo.com.ar>
2015-05-23 02:20:22 -0300

    Event API adjustment.

--- routes/event.js
+++ routes/event.js
@@ -5,10 +5,7 @@ var endpoint = require('../app/endpoint');
 router.get('/event/:name/:args(*)', function(req, res) {
     var e = endpoint('http', 'get', req.params.name);
     if (!e) return res.status(400).send('The requested endpoint has not been defined.');
-    e({
-        args: req.params.args,
-        body: req.body
-    });
+    e(req, res, req.params.args);
 });
 
 module.exports = router;
