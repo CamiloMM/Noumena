5575b6e4dc7c04bdd9e58ca27f572854b065177f
CamiloMM <herzschaltung@yahoo.com.ar>
2015-05-22 23:54:37 -0300

    Endpoints now are derived from a centralized constructor.

--- app/endpoint.js
+++ app/endpoint.js
@@ -7,7 +7,19 @@ var db = require('./db.js');
 // Actual endpoints are stored in this object.
 var endpoints = {};
 
-// Calling endpoint as a function will return an endpoint instance or throw.
+// Endpoints are built from this constructor.
+function Endpoint(routine) {
+    if (typeof routine != 'function') throw new Error('routine must be a function');
+    // The req and res are from express, and args is the substring from the URL
+    // that can contain whatever arguments the event may take.
+    return function(req, res, args) {
+        // I'm just shielding the underlying routine here because I don't
+        // know if I'll make changes to this API yet.
+        routine(req, res, args);
+    };
+}
+
+// Calling endpoint as a function will return an endpoint instance or null.
 function endpoint(protocol, method, name) {
     // Accept first argument to be 'protocol/method/name'.
     if (!name) {
@@ -19,14 +31,14 @@ function endpoint(protocol, method, name) {
     // Normalize method to uppercase.
     method = method.toUpperCase();
 
-    // This will throw if it doesn't exist.
-    endpoints[protocol][method][name].mustExist;
-
-    return endpoints[protocol][method][name];
+    try {
+        // This will throw if it doesn't exist.
+        endpoints[protocol][method][name].mustExist;
+        return endpoints[protocol][method][name];
+    } catch (e) {
+        return null;
+    }
 };
-
-// Expose DB to endpoints. We may revisit this approach if it's too crude.
-endpoint.db = db;
 
 // Registers an endpoint with a given name, for a specific method of a protocol.
 // An endpoint may not be registered twice; unless options.[ignore|override] are true.
@@ -53,7 +65,7 @@ endpoint.register = function(protocol, method, name, endpoint, options) {
     if (!endpoints[protocol][method]) endpoints[protocol][method] = {};
 
     if (!endpoints[protocol][method][name]) {
-        endpoints[protocol][method][name] = endpoint;
+        endpoints[protocol][method][name] = new Endpoint(endpoint);
     } else {
         // In this case we'll either throw, ignore or override the endpoint.
         switch (true) {
@@ -62,7 +74,7 @@ endpoint.register = function(protocol, method, name, endpoint, options) {
                 break;
             // Write the given endpoint over the existing one.
             case !!options.override:
-                endpoints[protocol][method][name] = endpoint;
+                endpoints[protocol][method][name] = new Endpoint(endpoint);
                 break;
             // Make a scandal about it.
             default:
@@ -73,5 +85,8 @@ endpoint.register = function(protocol, method, name, endpoint, options) {
         }
     }
 };
+
+// Expose DB to endpoints. We may revisit this approach if it's too crude.
+endpoint.db = db;
 
 module.exports = endpoint;
