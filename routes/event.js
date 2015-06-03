c62ce53dabd4c37c42913df7f74c00094db7ec9f
CamiloMM <herzschaltung@yahoo.com.ar>
2015-06-03 18:13:34 -0300

    Failsafe.

--- routes/event.js
+++ routes/event.js
@@ -5,7 +5,7 @@ var endpoint = require('../app/endpoint');
 router.get('/event/:name/:args(*)', function(req, res) {
     var e = endpoint('http', 'get', req.params.name);
     if (!e) return res.status(400).send('The requested endpoint has not been defined.');
-    e(req, res, req.params.args);
+    e(req, res, req.params.args || '');
 });
 
 module.exports = router;
