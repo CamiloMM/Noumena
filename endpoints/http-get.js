d84a5056848e1de32f8ceb6530905a4cb713f1d8
CamiloMM <herzschaltung@yahoo.com.ar>
2015-06-03 18:51:14 -0300

    Much simpler endpoint thanks to better API design.

--- endpoints/http-get.js
+++ endpoints/http-get.js
@@ -9,14 +9,8 @@ endpoint.register('http/GET/hg', function(req, res, args) {
     endpoint.parseArgs(args, null, req, 'http-get', function(err, parsed) {
         if (err) return res.sendStatus(500);
         if (!parsed) return res.sendStatus(400);
-        var project  = parsed.project,
-            category = parsed.category,
-            action   = parsed.action;
-        function callback(error) { res.sendStatus(error ? 500 : 200); }
-        if (parsed.data) {
-            endpoint.db.registerDataEvent(project, category, action, parsed.data, callback);
-        } else {
-            endpoint.db.registerSimpleEvent(project, category, action, callback);
-        }
+        endpoint.db.registerEvent(parsed, function(error) {
+            res.sendStatus(error ? 500 : 200);
+        });
     });
 });
