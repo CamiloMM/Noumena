0e6825ad2f7f5c875913884ff7d4cc1e91b81d2f
CamiloMM <herzschaltung@yahoo.com.ar>
2015-06-16 18:05:34 -0300

    Incorporate /projects into /manage, for views and routes.

--- routes/manage.js
+++ routes/manage.js
@@ -3,6 +3,17 @@ var router  = express.Router();
 var app     = require('../app.js');
 var db      = require('../app/db.js');
 
+router.get('/manage', app.adminOnly(function(req, res) {
+    db.getProjects(function(projects) {
+        if (!projects) req.flash('bad', 'Database error from db.getProjects!');
+        res.render('manage', {
+            title: '/',
+            mode: projects ? 'projects' : 'error',
+            projects: projects
+        });
+    });
+}));
+
 router.get('/manage/:project', app.adminOnly(function(req, res) {
     db.getCategories(req.params.project, function(categories) {
         if (!categories) req.flash('bad', 'Invalid project!');
